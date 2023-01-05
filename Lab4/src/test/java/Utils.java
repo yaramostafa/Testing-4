9cc9f1c8d4fcea8e73abfc649c8ea154f376bded
yara <89746218+yaramostafa@users.noreply.github.com>
2023-01-05 17:05:00 +0200

    Update Utils.java

--- Lab4/src/test/java/Utils.java
+++ Lab4/src/test/java/Utils.java
@@ -1,3 +1,4 @@
+#for chrome drivers
 public class Utils {
     final static String BASE_URL = "https://formy-project.herokuapp.com/form";
     final static String CHROME_DRIVER_LOCATION = "chromedriver";
